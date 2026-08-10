7dd246c84d42ef347118348e9a0f78a300ff271c
Vegekku <vegekku@gmail.com>
2026-08-10 10:36:13 +0200

    chore: move privacy.html to docs/, update build and options link, add tabs permission

--- build.js
+++ build.js
@@ -15,7 +15,7 @@ mkdirSync("dist/icons", { recursive: true });
 
 const staticFiles = [
   "icons/icon16.png", "icons/icon48.png", "icons/icon128.png",
-  "src/popup.html", "src/options.html", "src/privacy.html",
+  "src/popup.html", "src/options.html",
   "src/styles/styles.css", "src/styles/popup.css", "src/styles/options.css",
   "manifest.json",
 ];
